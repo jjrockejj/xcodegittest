356fa5a8f90c2be59d8556e9be799a3d2faadc70
John Grooms <johnw.grooms@yahoo.com>
2019-12-12 10:49:36 -0500

    Initial Commit

---
+++ gitexample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  gitexample
+//
+//  Created by John Grooms on 12/12/19.
+//  Copyright © 2019 John Grooms. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
